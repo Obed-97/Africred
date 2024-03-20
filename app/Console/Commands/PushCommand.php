2ffa5a562084c2661b5a00bb727669b7378c6902
sis3 <sanoko66@@gmail.com>
2024-03-20 13:46:00 +0000

    web push notif pret, solder

--- app/Console/Commands/PushCommand.php
+++ app/Console/Commands/PushCommand.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Credit;
+use App\Notifications\PushNotif;
+use App\Services\Tool;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class PushCommand extends Command
 {
@@ -11,7 +16,7 @@ class PushCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:push-notification';
+    protected $signature = 'command:push_notification';
 
     /**
      * The console command description.
@@ -27,6 +32,43 @@ class PushCommand extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $tool = new Tool();
+        $carbon = new Carbon();
+
+        $pr = 0;
+        $ps = 0;
+
+        $credits = Credit::where('statut', 'Accordé')->get();
+
+        foreach($credits as $credit){
+
+            $enDate = $carbon->parse($credit->date_fin);
+            $encours =  $tool->encours_actualiser($credit->id);
+
+            if ($encours > 0){
+                if($enDate->isPast()){
+                    $pr += 1;
+                }
+            }
+
+            if ($encours == 0){
+                $ps += 1;
+            }
+
+        }
+
+        $pr = new PushNotif(
+            'Prêts en retard',
+            'Il y a '. $pr. ' prêts en retard !'
+        );
+        $tool->pushNotif($tool->managerUsers(), $pr);
+
+        $pr = new PushNotif(
+            'Prêts en solder',
+            'Il y a '. $ps. ' prêts en solder !'
+        );
+        $tool->pushNotif($tool->managerUsers(), $pr);
+
+
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('push-notification')->dailyAt('16:30');
+        $schedule->command('command:push_notification')->dailyAt('16:30');
     }
 
     /**
